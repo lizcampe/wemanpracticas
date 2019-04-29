f1b7c3c2d735007614f2ffc64af5682cf84cb1b1
lizcampe <lizcampe@gmail.com>
2019-04-29 01:09:48 -0500

    VA

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 //Esta variable guarda una referencia al control de resultado, se declara global para no tener que estar ejecutando document.getElementById en cada ocasión que se necesite
-let ctrlMostrado = document.getElementById('salida');
+let ctrlMostrado = document.getElementById("salida").innerHTML;
 
-function dibujaTarjetas(arreglo) {
-	let imprime = "<table id='registro'><tr><th>Habitación</th><th>Huesped</th></tr>";
+var registro = [
+	{numHabitacion: "87", nombreHuesped: "Donna Noble"},
+	{numHabitacion: "34", nombreHuesped: "Jean Luc Piccard"},
+	{numHabitacion: "23", nombreHuesped: "Deana Troi"},
+	{numHabitacion: "12", nombreHuesped: "Christpher Pike"},
+	{numHabitacion: "67", nombreHuesped: "Amy Pond"},
+	{numHabitacion: "45", nombreHuesped: "Miyamoto Musashi"},
+	{numHabitacion: "93", nombreHuesped: "Amelia Earhart"},
+	{numHabitacion: "47", nombreHuesped: "Katherin Janeway"}
+];
+
+function dibujaTarjetas (arreglo) {
+	let imprime = "<table id= 'registro'> <tr> <th>Habitación</th> <th>Huesped</th></tr>";
 	//La siguiente estructura de for es casi ya un arcaísmo. Sin embargo, los clásicos nunca mueren.
 	for (let i = 0; i < arreglo.length; i++) {
 		imprime += "<tr><td>" + arreglo[i].numHabitacion + "</td><td>" + arreglo[i].nombreHuesped + "</td></tr>";
@@ -11,24 +22,28 @@ function dibujaTarjetas(arreglo) {
 	ctrlMostrado.innerHTML = imprime;
 }
 
+
 function ordenaPorHabitacion(registro) {
-	if (registro.length<=1){
+		if (registro.length<=1){
 		return registro;
-	} else {
+	} 
+	else {
 		var pivot = Math.floor(Math.random()* registro.length-1);
 		var max = [];
 		var min = [];
 		var p = [];
-		for (var i = 0; registro.length;i++){
-			if (registro[pivot].numHabitacion > registro[i].numHabitacion) {
-				min.push(registro[i].numHabitacion);
-			} else if (registro[pivot].numHabitacion < registro[i].numHabitacion) {
-				max.push(registro[i].numHabitacion);
-			} else {
-				p.push (registro[i].numHabitacion);
+
+    for (var i = 0; registro.length;i++){
+			if (registro.numHabitacion[pivot] > registro.numHabitacion[i] {
+				min.push(registro.numHabitacion[i]);
+			}  if (registro.numHabitacion[pivot] < registro.numHabitacion[i]) {
+				max.push(registro.numHabitacion[i]);
+			}  else if (registro.numHabitacion[pivot] = registro.numHabitacion)[i] {
+				p.push (registro.numHabitacion[i]);
 			}
-		}
-		return(ordenaPorHabitacion(min)).concat(p, ordenaPorHabitacion(max));
+
+	
+			return(ordenaPorHabitacion(min)).concat(p, ordenaPorHabitacion(max));
 	}
 	//En esta función escribe la función que ordenará el arreglo por habitación
 
